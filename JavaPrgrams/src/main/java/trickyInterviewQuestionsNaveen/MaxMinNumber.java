b1b8e05933db2b8550db225bd67bc232d2d82016
pandu.damera <pandu.damera@DAMERP1-TL2.thcg.net>
2021-12-08 09:56:17 +0530

    Java Interview Programs

---
+++ JavaPrgrams/src/main/java/trickyInterviewQuestionsNaveen/MaxMinNumber.java
@@ -0,0 +1,38 @@
+package trickyInterviewQuestionsNaveen;
+
+public class MaxMinNumber {
+
+	// Find out Max and Min number out of three positive numbers 
+	
+	public static void main(String[] args) {
+		findMaxNumber(20, 30, 60);
+		findMinNumber(30, 70, 100);
+
+	}
+	
+	public static void findMaxNumber(int a, int b, int c)
+	{
+		int max=0;
+		while(a>0 || b>0 || c>0)
+		{
+			a--;
+			b--;
+			c--;
+			max++;
+		}
+		System.out.println(max);
+	}
+	public static void findMinNumber(int a, int b, int c)
+	{
+		int min=0;
+		while(a>0 && b>0 && c>0)
+		{
+			a--;
+			b--;
+			c--;
+			min++;
+		}
+		System.out.println(min);
+	}
+
+}
---
+++ JavaPrgrams/src/main/java/trickyInterviewQuestionsNaveen/PrintWithBlankMain.java
@@ -0,0 +1,18 @@
+package trickyInterviewQuestionsNaveen;
+
+public class PrintWithBlankMain {
+
+	// Print hello world with out static block and blank main method
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+public static int printString()
+{
+	System.out.println("Hello World");
+	return 0;
+}
+
+static int p=printString(); // Boolean 
+}
